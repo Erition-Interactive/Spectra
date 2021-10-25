8826f30fde459f45a2a1c10ac1fc42b7a24910dc
0Nera <slizen.can@gmail.com>
2021-10-25 14:48:38 +0300

    Move all parse functions, add json loader

--- Spectra/AudioListen.py
+++ Spectra/AudioListen.py
@@ -1,32 +1,18 @@
 import pyttsx3
 import speech_recognition as sr
 from . import ExecuteManager
+from . import Parser
 
+Parse = Parser.Parser()
 Execute = ExecuteManager.Execute(debug=True)
 
-Actions = [
-    {
-        "examples": ["привет", "здравствуй", "добрый день",
-            "hello", "good morning"],
-        "responses": "print('Ну привет')"
-    },
-    {
-        "examples": ["пока", "до свидания", "увидимся", "до встречи",
-            "goodbye", "bye", "see you soon"],
-        "responses": "exit()"
-    },
-    {
-        "examples": ["найди в гугл",
-            "search on google", "google", "find on google"],
-        "responses": "print('Гугл тебе заблокировали? Сам ищи')"
-    }
-]
+
 
 def record_volume():
     r = sr.Recognizer()
     with sr.Microphone(device_index = 1) as source:
         print('Настраиваюсь.')
-        r.adjust_for_ambient_noise(source, duration=2) #настройка посторонних шумов
+        r.adjust_for_ambient_noise(source, duration=1) #настройка посторонних шумов
         print('Слушаю...')
         audio = r.listen(source)
     print('Услышала.')
@@ -34,11 +20,9 @@ def record_volume():
         query = r.recognize_google(audio, language = 'ru-RU')
         text = query.lower()
         print(f'Вы сказали: {query.lower()}')
-        for i in Actions:
-            for x in i['examples']:
-                if x.lower() in text:
-                    Execute.Exec(action=i['responses'], data=None)
-                    break
+        parsed = Parse.Parse(data=text)
+        Execute.Exec(action=parsed[0], data=parsed[1])
+        
     except Exception as E:
         print('AudioListener error: {}'.format(E))
 
--- Spectra/Parser.py
+++ Spectra/Parser.py
@@ -1,32 +1,22 @@
-Actions = [
-    {
-        "examples": ["привет", "здравствуй", "добрый день",
-            "hello", "good morning"],
-        "responses": "print('Ну привет')"
-    },
-    {
-        "examples": ["пока", "до свидания", "увидимся", "до встречи",
-            "goodbye", "bye", "see you soon"],
-        "responses": "exit()"
-    },
-    {
-        "examples": ["найди в гугл",
-            "search on google", "google", "find on google"],
-        "responses": "print('Гугл тебе заблокировали? Сам ищи')"
-    }
-]
+from . import ExecuteManager
+import json
+
+Actions = []
+
+with open('Data/Actions.json') as f:
+    Actions = json.load(f)
+
+
+Execute = ExecuteManager.Execute(debug=True)
+
 
 class Parser(object):
     def __init__(self):
         pass
     
     def Parse(self, data):
-        InputData = data.strip().split(" ")
-        print(InputData)
         for i in Actions:
-            if i['examples'] in InputData:
-                print(i['responses'])
-                return {
-                    'Action': i['responses'], 
-                    'Data': InputData.pop(0)
-                    }
+            for x in i['examples']:
+                if x.lower() in data:
+                    return [i['responses'], None]
+        return ["null", None]
