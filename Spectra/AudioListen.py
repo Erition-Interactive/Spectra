0199de879f8ccc6923aff20f639255d951e9dc77
0Nera <slizen.can@gmail.com>
2021-10-25 13:46:51 +0300

    Add audio listener

--- Spectra/AudioListen.py
+++ Spectra/AudioListen.py
@@ -1,5 +1,23 @@
+import pyttsx3
+import speech_recognition as sr
 
 #async def Listen():
 def Listen():
     print("Listen")
-    return "1"
+    def record_volume():
+        r = sr.Recognizer()
+        with sr.Microphone(device_index = 1) as source:
+            print('Настраиваюсь.')
+            r.adjust_for_ambient_noise(source, duration=0.5) #настройка посторонних шумов
+            print('Слушаю...')
+            audio = r.listen(source)
+        print('Услышала.')
+        try:
+            query = r.recognize_google(audio, language = 'ru-RU')
+            text = query.lower()
+            print(f'Вы сказали: {query.lower()}')
+        except:
+            print('Error')
+
+        while True:
+            record_volume()
